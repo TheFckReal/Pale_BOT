4494bc8ee4ccbd053ba9b2c4b0dce2a16929a198
The Real <sashasan2003@mail.ru>
2023-08-27 03:33:33 +0300

    Added support release and debug vers

--- Zone34_BOT/CacheSingleton.cs
+++ Zone34_BOT/CacheSingleton.cs
@@ -6,13 +6,18 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
+
 namespace Pale_BOT
 {
     class CacheSingleton
     {
         private static MemoryCache? _cache;
         private static object _lock = new object();
+#if RELEASE
+        private static MemoryCacheOptions _memoryCacheOptions { get; } = JsonSerializer.Deserialize<MemoryCacheOptions>(File.ReadAllText("Configs\\cacheOptions.cfg")) ?? new MemoryCacheOptions();
+#else
         private static MemoryCacheOptions _memoryCacheOptions { get; } = JsonSerializer.Deserialize<MemoryCacheOptions>(File.ReadAllText("..\\..\\..\\Configs\\cacheOptions.cfg")) ?? new MemoryCacheOptions();
+#endif
         private CacheSingleton() { }
         public static MemoryCacheEntryOptions GetStandartCacheEntryOptions()
         {
